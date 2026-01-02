047f5a3ab598c0aafa479eecc1c0291567a51a1f
Manas17012006 <mkawathalkar@gmail.com>
2026-01-03 03:41:55 +0530

    p

--- middleware/auth.mw.js
+++ middleware/auth.mw.js
@@ -1,14 +1,24 @@
+const userModel = require('../models/userModel.model');
+const jwt = require('jsonwebtoken');
+
 const userauth = async (req, res, next) => {
   try {
+    // Get token from Authorization header
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token)
+    if (!token) {
       return res.status(401).json({ success: false, message: "Please login to get started!" });
+    }
 
-    // find user by token
-    const user = await userModel.findOne({ token });
-    if (!user)
+    // Decode token (if using JWT)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Find user by token stored in DB
+    const user = await userModel.findOne({ _id: decoded.id, token: token });
+    if (!user) {
       return res.status(401).json({ success: false, message: "Invalid token" });
+    }
 
+    // Set req.userId for next handlers
     req.userId = user._id;
     next();
   } catch (err) {
